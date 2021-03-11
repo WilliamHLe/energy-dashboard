cf9611f2f5b5a77c54b5da35c1b22c5de6f1706e
williahl <william.h.le@ntnu.no>
2021-03-11 16:30:37 +0100

    #15 #14 Fix

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,15 +1,22 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    rules: {
-        '@typescript-eslint/no-var-requires': 0,
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from 'express';
+import mongoose from 'mongoose';
 
 // Example mongodb connection
 mongoose.connect(
-    `mongodb://${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
+  `mongodb://${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
 );
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => console.log("connected"));
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('connected'));
 
 // Example express server
 const app = express();
 const port = 3000;
 
-app.get("/", (req: any, res: { send: (arg0: string) => void; }) => {
-    res.send("DB_HOST" + process.env.DB_HOST);
+app.get('/', (req, res) => {
+  res.send(`DB_HOST${process.env.DB_HOST}`);
 });
 
-app.listen(port, (err: any) => {
-    if (err) {
-        return console.error(err);
-    }
-    return console.log(`server is listening on ${port}`);
+app.listen(port, () => {
+  console.log(`express is listening on port ${port}`);
 });
