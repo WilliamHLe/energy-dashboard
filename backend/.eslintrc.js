735704264cdb2f9e2de87593204a95ac61c1c8a8
williahl <william.h.le@ntnu.no>
2021-03-11 17:17:58 +0100

    #15 #14 Fix v2

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,19 +4,16 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    'airbnb-base',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: [
-    'react',
     '@typescript-eslint',
   ],
+  rules: {
+  },
 };
