403f30883f8764b91d83d7df8adb050aca4f6341
Jane <jane@nhi.no>
2021-03-18 12:03:41 +0100

    Info component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import style from './navbar.module.css';
 
 export default function Navbar() {
   return (
-    <div className={style.container}>
+    <nav className={style.container}>
       <NavLink className={`${style.menuitems} ${style.oversikt}`} activeClassName={`${style.selected} ${style.oversiktF}`} exact to="/">Oversikt</NavLink>
       <NavLink className={`${style.menuitems} ${style.skoler}`} activeClassName={`${style.selected} ${style.skolerF}`} to="/skoler">Skoler</NavLink>
 
@@ -17,6 +17,6 @@ export default function Navbar() {
       <NavLink className={`${style.menuitems} ${style.andre}`} activeClassName={`${style.selected} ${style.andreF}`} to="/abygg">Andre bygg</NavLink>
 
       <NavLink className={`${style.menuitems} ${style.energi}`} activeClassName={`${style.selected} ${style.energiF}`} to="/energitips">Energitips</NavLink>
-    </div>
+    </nav>
   );
 }
