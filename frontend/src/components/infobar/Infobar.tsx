6fbc27a64941827f0b2c7aeeb222f21459b212e2
Jane <jane@nhi.no>
2021-04-03 14:55:27 +0200

    Infobar

--- frontend/src/components/infobar/Infobar.tsx
+++ frontend/src/components/infobar/Infobar.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { useParams } from 'react-router';
 import InfoItem from './InfoItem';
-import Icon from './icons/noto_house.png';
+import Buildings from './icons/noto_house.png';
+import Energy from './icons/energy.svg';
+import Squaremeters from './icons/Group 223.svg';
+import Energygoal from './icons/Group 222.svg';
+import style from './infoItem.module.css';
 
-const InfoBar = () => (
-  <div>
-    <div className="">
-      <InfoItem heading="660" description="Bygg som overvåkes" icon={Icon} />
+interface building {
+  energyUsed: number,
+  energySaved: number,
+  buildingArea: number,
+  buildings: number
+}
+// mock data
+const data:building = {
+  energyUsed: 1234567,
+  energySaved: 1234567,
+  buildingArea: 1234567,
+  buildings: 1234,
+};
+
+const InfoBar = () => {
+  const { category } = useParams<{category:string | undefined}>();
+  return (
+    <div className={style.infobar}>
+      <p className={style.categoryName}>{category?.toUpperCase() || 'TRONDHEIM KOMMUNE'}</p>
+      <InfoItem heading={`${data.energyUsed} Kwh`} description="Brukt så langt i år" icon={Energy} />
+      <InfoItem heading="utregning %" description="Spart så langt sammenlignet med i fjor" icon={Energygoal} />
+      <InfoItem heading={`${data.buildingArea} m2`} description="Energioppfølges Trondheim kommune" icon={Squaremeters} />
+      <InfoItem heading={data.buildings.toString()} description="Bygg som overvåkes" icon={Buildings} />
     </div>
-  </div>
-);
-
+  );
+};
 export default InfoBar;
