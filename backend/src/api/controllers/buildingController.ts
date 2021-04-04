2ae179b0f9b3b85e258e521bc9abc970b28eae51
WilliamHLe <le.william.h@outlook.com>
2021-04-05 00:34:04 +0200

    #142 Change of query

--- backend/src/api/controllers/buildingController.ts
+++ backend/src/api/controllers/buildingController.ts
@@ -4,40 +4,52 @@ import Sensors from '../models/sensors.model';
 
 const getTotalEnergyBuilding = async (req: Request, res: Response, next: NextFunction) => {
   const buildingId = mongoose.Types.ObjectId(req.params.id);
-  const fromDate = `${new Date(req.query.from_date as string).toISOString().split('.')[0]}0Z`;
-  // const toDate = req.query.to_date;
+  const fromDate = req.query.from_date;
+  const toDate = req.query.to_date;
 
-  const buildings = await Sensors.find({
-    building: buildingId,
-    unit_of_measurement: 'kWh',
-  }).select({
-    measurements: {
-      $filter: {
-        input: '$measurements',
-        as: 'measurement',
-        cond: {
-          $gte: ['$$measurement.date', fromDate],
-          // $lte: [ "$$measurement.date", new Date(toDate as string).toISOString() ]
+  const query = [
+    {
+      $match: {
+        building: buildingId,
+        unit_of_measurement: 'kWh',
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        total: {
+          $sum: { $sum: '$measurements.measurement' },
         },
       },
     },
-  });
+  ];
 
-  const totalEnergyBuilding = () => {
-    let energy = 0;
-    for (let i = 0; i < buildings.length; i += 1) {
-      for (let j = 0; j < buildings[i].measurements.length; j += 1) {
-        energy += buildings[i].measurements[j].measurement;
-      }
-    }
-    return energy;
-  };
+  // Check if query parameter fromDate or toDate exists, if yes add filter query
+  if (fromDate || toDate) {
+    const filter: any = {
+      $project: {
+        measurements: {
+          $filter: {
+            input: '$measurements',
+            as: 'measurement',
+            cond: {
+              $and: [
+                fromDate ? { $gte: ['$$measurement.date', new Date(fromDate as string)] } : {},
+                toDate ? { $lte: ['$$measurement.date', new Date(toDate as string)] } : {},
+              ],
+            },
+          },
+        },
+      },
+    };
+
+    query.splice(1, 0, filter);
+  }
+
+  const buildings = await Sensors.aggregate(query);
 
   if (buildings) {
-    res.json({
-      total: totalEnergyBuilding(),
-      // buildings
-    });
+    res.send(buildings);
   } else {
     next('Unable to retrieve measurement');
   }
