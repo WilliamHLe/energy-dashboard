63f2ec83343ea83539524d415bbe5165f6d00241
Jane <jane@nhi.no>
2021-04-06 13:52:06 +0200

    Auto stash before cherry pick of "Top list"

--- frontend/src/components/buildingCategoryOverview/BuildingCategoryOverview.tsx
+++ frontend/src/components/buildingCategoryOverview/BuildingCategoryOverview.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './category.module.css';
 import CategoryUsage from './graphs/CategoryUsage';
+import EnergyCarriers from './graphs/EnergyCarriers';
 import CategoryTopList from './topList/CategoryTopList';
 
 function BuildingCategoryOverview() {
@@ -11,6 +12,8 @@ function BuildingCategoryOverview() {
       </div>
       <CategoryTopList />
       <div className={`container ${style.energyCarriers}`} />
+        <EnergyCarriers />
+      </div>
       <div className={`container ${style.tips}`} />
     </div>
   );
