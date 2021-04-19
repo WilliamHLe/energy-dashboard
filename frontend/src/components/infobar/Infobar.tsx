e00d4c1273d07d10a2a46f4adf46a5d61264d0ee
Sengoh <sihognes@gmail.com>
2021-04-19 14:08:08 +0200

    Added API to infobar (#199)

--- frontend/src/components/infobar/Infobar.tsx
+++ frontend/src/components/infobar/Infobar.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import axios from 'axios';
 import InfoItem from './InfoItem';
 import Buildings from './icons/noto_house.png';
 import Energy from './icons/energy.svg';
@@ -10,25 +11,38 @@ import style from './infoItem.module.css';
 interface building {
   energyUsed: number,
   energySaved: number,
-  buildingArea: number,
+  area: number,
   buildings: number
 }
-// mock data
-const data:building = {
-  energyUsed: 1234567,
-  energySaved: 1234567,
-  buildingArea: 1234567,
-  buildings: 1234,
-};
 
 const InfoBar = () => {
   const { category } = useParams<{category:string | undefined}>();
+  const [data, setData] = useState<building>({
+    energyUsed: 0,
+    energySaved: 0,
+    area: 0,
+    buildings: 0,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (category) {
+        const results = await axios.get(`/metrics/${category}`);
+        setData(results.data);
+      } else {
+        const results = await axios.get('/metrics');
+        setData(results.data);
+      }
+    };
+    fetchData();
+  }, [category]);
+
   return (
     <div className={style.infobar}>
       <h1 className={style.categoryName}>{category?.toUpperCase() || 'TRONDHEIM KOMMUNE'}</h1>
       <InfoItem heading={`${data.energyUsed} Kwh`} description="Brukt så langt i år" icon={Energy} />
-      <InfoItem heading="utregning %" description="Spart så langt sammenlignet med i fjor" icon={Energygoal} />
-      <InfoItem heading={`${data.buildingArea} m2`} description="Energioppfølges Trondheim kommune" icon={Squaremeters} />
+      <InfoItem heading={`${data.energySaved} %`} description="Spart så langt sammenlignet med i fjor" icon={Energygoal} />
+      <InfoItem heading={`${data.area} m2`} description="Energioppfølges Trondheim kommune" icon={Squaremeters} />
       <InfoItem heading={data.buildings.toString()} description="Bygg som overvåkes" icon={Buildings} />
     </div>
   );
