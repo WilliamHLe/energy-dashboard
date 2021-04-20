ad432f94233731ea53e7e6b99081659114347dfe
Jane <jane@nhi.no>
2021-04-20 09:41:20 +0200

    Added text

--- frontend/src/components/energyTips/EnergyTips.tsx
+++ frontend/src/components/energyTips/EnergyTips.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
 import React, { useState } from 'react';
 import style from './energyTips.module.css';
 import SideButton from './SideButton';
@@ -24,11 +24,34 @@ const EnergyTips = () => {
           <div className={style.energyTipText}>
             <h1>{data[selectedTopic].title}</h1>
             <p>{data[selectedTopic].description}</p>
+            <p>{`${data[selectedTopic].title} egner seg for deg som:` }</p>
+            <ul>
+              {data[selectedTopic].who?.map((punkt) => (
+                <li>
+                  {punkt}
+                </li>
+              ))}
+            </ul>
+            <p>{`Gode grunner til å montere ${data[selectedTopic].title}:` }</p>
+            <ul>
+              {data[selectedTopic].why?.map((punkt) => (
+                <li>
+                  {punkt}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
         <div className={`container ${style.twoPercent}`}>
           <div className={style.energyTipText}>
             <h1>2% målet</h1>
+            <p>
+              2% målet er et mål om at
+              alle bygg i kommunen skal bruke 2% mindre energi enn året før. Om vi alle gjør dette kan
+              vi spare store mengder energi og dermed skape en mer klimavennlig og bærekraftig by, i
+              tillegg til at vi tar et steg i riktig retning for å stoppe klimaendringene og få en
+              grønnere og friskere planet.
+            </p>
           </div>
         </div>
         <div className={style.niceFigure}>
