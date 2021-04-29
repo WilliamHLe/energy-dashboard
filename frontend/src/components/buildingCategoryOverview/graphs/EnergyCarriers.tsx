b411313ad7c608fa91d9623a77c4bf16ec85e11e
Sengoh <sihognes@gmail.com>
2021-04-29 16:26:06 +0200

    Added percentage to the pie chart

--- frontend/src/components/buildingCategoryOverview/graphs/EnergyCarriers.tsx
+++ frontend/src/components/buildingCategoryOverview/graphs/EnergyCarriers.tsx
@@ -59,16 +59,13 @@ function EnergyCarriers() {
       pie: {
         allowPointSelect: true,
         cursor: 'pointer',
+        showInLegend: true,
         dataLabels: {
           enabled: false,
         },
-        showInLegend: true,
       },
       series: {
         style: {
-          color: 'white',
-        },
-        dataLabels: {
           color: 'white',
         },
       },
@@ -77,6 +74,7 @@ function EnergyCarriers() {
       itemStyle: {
         color: 'white',
       },
+      labelFormat: '{name} ({percentage:.1f}%)',
     },
     series: data,
   };
